f706e6024e5f5d4b22a5777a6d74768ce65e0594
levelfour <levelfourslv@gmail.com>
2015-05-23 01:34:53 +0900

    simple option parsing

--- src/pdp11.ml
+++ src/pdp11.ml
@@ -6,22 +6,24 @@ let _ =
     if Array.length Sys.argv < 2 then
         printf "Too few args\n"
     else
-        let ic = open_in_bin Sys.argv.(1) in
+        let dmode = if Sys.argv.(1) = "-d" then true else false in
+        let ic = open_in_bin Sys.argv.(if dmode then 2 else 1) in
         try
             let bin = new a_out_format (read_bin ic) in
-            let rec disasall binary =
-                try
-                    let (pc, bytecode, asm) = binary#disas in
-                    let hexcode bl =
-                        String.concat " " (List.map (fun x -> sprintf "%04x" x) bl)
-                    in
-                    printf "%+4x:\t%-*s %s\n" pc 15 (hexcode bytecode) asm;
-                    disasall binary;
-                with OutOfRange ->
-                    ()
-            in
-            printf "TextSize=%d(B), DataSize=%d(B)\n" bin#text_size bin#data_size;
-            disasall bin;
+            if dmode then
+                let rec disasall binary =
+                    try
+                        let (pc, bytecode, asm) = binary#disas in
+                        let hexcode bl =
+                            String.concat " " (List.map (fun x -> sprintf "%04x" x) bl)
+                        in
+                        printf "%+4x:\t%-*s %s\n" pc 15 (hexcode bytecode) asm;
+                        disasall binary;
+                    with OutOfRange -> ()
+                in
+                disasall bin;
+            else
+                printf "simulator mode\n";
             close_in ic
         with e ->
             close_in_noerr ic;
